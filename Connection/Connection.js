8c0562c5cd348f31020315028c0799ec756c6618
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2024-01-08 09:10:47 -0300

    created connection class

---
+++ Connection/Connection.js
@@ -0,0 +1,140 @@
+export class Connection {
+    URL;
+    params;
+    pathFile;
+    err = {
+        error:false,
+        exception:''
+    };
+
+    async get(params, pathFile, err) {
+        this.validationParams(params, pathFile, err);
+        await this.settingUrl(`/Controller/${this.pathFile}?app_id=3&AUTH=`, params)
+        let req;
+        await fetch(this.URL, {
+            method: 'GET',
+        }).then(response => response.json())
+            .then(body => {
+                if (body.error) throw new Error(body.message)
+                req = body;
+            }).catch(messageErr => {
+                req = this.prepareCatchReturn(messageErr);
+            })
+        this.cleanParams();
+        return req;
+    }
+
+    async post(params, pathFile, err) {
+        this.validationParams(params, pathFile, err);
+        await this.settingUrl(`/Controller/${this.pathFile}?app_id=3&AUTH=`)
+        let req;
+        await fetch(this.URL, {
+            method: 'POST',
+            body: JSON.stringify(this.params)
+        }).then(response => response.json())
+            .then(body => {
+                if (body.error) throw Error(body.message);
+                req = body;
+            }).catch(messageErr => {
+                req = this.prepareCatchReturn(messageErr);
+            })
+        this.cleanParams();
+        return req;
+    }
+
+    async postLogin(params, pathFile, err) {
+        this.validationParams(params, pathFile, err);
+        await this.settingUrl(`/Controller/${this.pathFile}?app_id=3&login=&AUTH=`)
+        let req;
+        await fetch(this.URL, {
+            method: 'POST',
+            body: JSON.stringify(this.params)
+        }).then(response => response.json())
+            .then(body => {
+                if (body.error) throw Error(body.message);
+                req = body;
+            }).catch(messageErr => {
+                req = this.prepareCatchReturn(messageErr);
+            })
+        this.cleanParams();
+        return req;
+    }
+
+    async put(params, pathFile, err) {
+        this.validationParams(params, pathFile, err);
+        await this.settingUrl(`/Controller/${this.pathFile}?app_id=3&AUTH=`)
+        let req;
+        await fetch(this.URL, {
+            method: 'PUT',
+            body: JSON.stringify(this.params)
+        }).then(response => response.json())
+            .then(body => {
+                if (body.error) throw Error(body.message)
+                req = body;
+            }).catch(messageErr => {
+                req = this.prepareCatchReturn(messageErr);
+            })
+        this.cleanParams();
+        return req;
+    }
+
+    async putDefaltPassw(params, pathFile, err) {
+        this.validationParams(params, pathFile, err);
+        await this.settingUrl(`/Controller/${this.pathFile}?app_id=3&login=&AUTH=`)
+        let req;
+        await fetch(this.URL, {
+            method: 'PUT',
+            body: JSON.stringify(this.params)
+        }).then(response => response.json())
+            .then(body => {
+                if (body.error) throw Error(body.message)
+                req = body;
+            }).catch(messageErr => {
+                req = this.prepareCatchReturn(messageErr);
+            })
+        this.cleanParams();
+        return req;
+    }
+
+    async delete(params, pathFile, err) {
+        this.validationParams(params, pathFile, err);
+        await this.settingUrl(`/Controller/${this.pathFile}?app_id=3&AUTH=`);
+        let req;
+        console.log(this.URL)
+        await fetch(this.URL, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Credentials": "true",
+                "Access-Control-Max-Age": "86400"
+            },
+            body: JSON.stringify(this.params)
+        }).then((response) => response.json())
+            .then((body) => {
+                if (body.error) throw Error(body.message);
+                req = body;
+            }).catch((messageErr) => {
+                req = this.prepareCatchReturn(messageErr);
+            });
+        this.cleanParams();
+        return req;
+    }
+    validationParams(params, pathFile, err) {
+        if (params) this.params = params;
+        if (pathFile) this.pathFile = pathFile;
+        if (err) this.err = err;
+    }
+    cleanParams() {
+        this.params = "";
+        this.pathFile = "";
+        this.err = "";
+    }
+    async settingUrl(middlewer, params) {
+        let server = "http://192.168.0.99:71/GLOBAL";
+        let token = localStorage.getItem("token");
+        this.URL = server + middlewer + token + (params ? params : "");
+    }
+    prepareCatchReturn(messageErr) {
+        return { "error": true, "message": messageErr["message"] }
+    }
+}
