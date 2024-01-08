495f46a52df74638ac6fb3183f0fe456970295d1
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2024-01-08 10:19:09 -0300

    Module for component and container component created

--- Main.js
+++ Main.js
@@ -1,5 +1,13 @@
+import Containers from "./Components/Containers.js";
+
 // Função auto excutável. Neste formato modular ela é a única função a ser excutada neste script.
-(()=>{
-        console.log("Hellow Word");
-    }
+(() => {
+    const section = document.querySelector("#containerMain section");
+    section.appendChild(login());
+}
 )();
+
+function login() {
+    const loginContainer = new Containers();
+    return loginContainer.containerBasic({ id: 'loginContainer' });
+}
