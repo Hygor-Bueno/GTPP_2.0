91dd76b85c94f32e257f8c8ca5c39f658631af36
ProgramacaoPgPs <programacaopegpese@gmail.com>
2024-01-08 10:26:35 -0300

    firt commit

--- Main.js
+++ Main.js
@@ -1,5 +1,17 @@
+import { modal } from "./components/Modal/modal.js";
+
 // Função auto excutável. Neste formato modular ela é a única função a ser excutada neste script.
 (()=>{
         console.log("Hellow Word");
+        let button = document.querySelector("#containerMain section button");
+        button.addEventListener("click",modal);
     }
 )();
+function mostrarModal() {
+    // Chama a função modal que cria e adiciona o modal à tela
+    modal();
+    
+    // Obtém o modal e o exibe
+    const modalElement = document.querySelectorAll('.modal');
+    modalElement.style.display = 'block';
+}
