89ed9bb0aec58dfe4c13f3c8b0db7f47ba6c9ca3
ProgramacaoPgPs <programacaopegpese@gmail.com>
2024-01-08 16:04:49 -0300

    sending modal component

--- Main.js
+++ Main.js
@@ -1,10 +1,12 @@
 import Containers from "./Components/Containers.js";
 import Form from "./Components/Form.js";
+import Modal from "./components/Modal/modal.js";
 
 // Função auto excutável. Neste formato modular ela é a única função a ser excutada neste script.
 (() => {
     const section = document.querySelector("#containerMain section");
     section.appendChild(login());
+    section.appendChild(Modals());
 }
 )();
 
@@ -17,4 +19,14 @@ function login() {
             element: form.ContainerForm({ classItemsForm: 'itemsForm',classForm:'form' })
         }
     );
+}
+
+function Modals() {
+    const modalContainer = new Containers();
+    let modal = new Modal();
+    
+    return modalContainer.containerBasic({
+        id: 'modalContainer',
+        element: modal.ContainerForm(),
+    })
 }
--- components/Modal/modal.js
+++ components/Modal/modal.js
@@ -1,24 +1,31 @@
 export default class Modal {
-    error(configs) {
-        try {
-            
-        } catch (error) {
-            console.error(error.message);
-        }
-    }
-    confirmation(configs) {
-        try {
-            
-        } catch (error) {
-            console.error(error.message);  
-        }
-    }
 
-    alert(configs) {
-        try {
-            
-        } catch (error) {
-          console.error(error.message);
-        }
+  ContainerForm(configs) {
+
+    const divModal = document.createElement('div');
+    if (configs && configs.classDivModal) divModal.className = configs.classDivModal;
+    divModal.appendChild(this.ModalDivision({title: 'Erro'}));
+    return divModal;
+  }
+
+  ModalDivision(configs) {
+    try {
+      if(!configs || !configs.title) {
+        throw new Error(`Keys not found`)
+      }
+      const divModalError = document.createElement('div');
+
+      divModalError.className = 'modal-gender'
+      divModalError.innerHTML = `
+        <h1>${configs.title}</h1>
+      `;
+
+      console.log(divModalError)
+
+      return divModalError;
+
+    } catch (error) {
+      console.error(error.message)
     }
+  }
 }
