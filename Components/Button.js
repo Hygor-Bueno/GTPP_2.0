5a4835a829e839f5ff48de70d25ad0029b015e13
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2024-01-10 16:48:12 -0300

    additions of comments in class Button

--- Components/Button.js
+++ Components/Button.js
@@ -1,7 +1,23 @@
 import Util from "../Util.js";
 
+/**
+ * Classe Button
+ * @date 1/10/2024 - 4:42:03 PM
+ * @author Hygor Bueno.
+ * @export
+ * @class Button
+ * @classdesc Um componente de botão criado para agilizar e padronizar os componentes da página. 
+ */
 export default class Button{
     util = new Util();
+    
+    /**
+     * Description placeholder
+     * @date 1/10/2024 - 4:44:45 PM
+     * @author Hygor Bueno.
+     * @param {Object} configs
+     * @returns {HTMLElement} Elemento Button
+     */
     Button(configs){
         try {
             let mandatory = ['type','title','description','onAction']; //Lista de chaves obrigatórias.
