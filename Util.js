3ab67c18670b83ff5277e6223873d58eb1fc02b6
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2024-01-11 09:32:57 -0300

    additions of comments in class Util.js

--- Util.js
+++ Util.js
@@ -1,4 +1,19 @@
+/**
+ * Classe destinada a funções utilitárias que serão utilizadas para todo o código.
+ * @date 1/11/2024 - 9:29:28 AM
+ * @author Hygor Bueno
+ * @export
+ * @class Util
+ */
 export default class Util {
+    /**
+     * Esse método é responsável por validar as chaves obrigatórias do componente.
+     * @date 1/11/2024 - 9:30:14 AM
+     *
+     * @param {[]} listKeys
+     * @param {object} object
+     * @returns {boolean}
+     */
     ValidatKeysComponent(listKeys,  object) {
         let result = true;
         listKeys.forEach(itemKey => {
@@ -7,6 +22,12 @@ export default class Util {
         });
         return result;
     }
+    /**
+     * Limpa todos os inputs de um determinado elemento.
+     * @date 1/11/2024 - 9:31:48 AM
+     *
+     * @param {string} local
+     */
     cleanInputs(local){
         let list = document.querySelectorAll(local);
         list.forEach(item=>item.value='');   
