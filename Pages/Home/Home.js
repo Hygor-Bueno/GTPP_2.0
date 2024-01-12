eca50e529382e22a6ca0c9d60d71181f2bb30e52
Hygor-Bueno <hygor_knd.x1@hotmail.com>
2024-01-12 09:20:11 -0300

    my first commit of day

--- Pages/Home/Home.js
+++ Pages/Home/Home.js
@@ -1,8 +1,9 @@
 import Card from "../../Components/Card.js";
 
-export function initHome () {
-    const card = new Card();
-    return card.createCard();
+export default class Home{
+    main(){
+        const card = new Card();
+        return card.createCard();
+    }
 }
 
-initHome();
