7930c056a7c2ca05d91b653beb0c0286a5a23d69
ProgramacaoPgPs <programacaopegpese@gmail.com>
2024-01-26 17:10:01 -0300

    send documentation with jsdocs

--- Components/Card.js
+++ Components/Card.js
@@ -6,11 +6,23 @@ import { CSVGenerator, PDFGenerator } from "./FileGenerator.js";
 import Modal from "./Modal.js";
 import { Connection } from "../Connection/Connection.js";
 
+/**
+ * Classe Card
+ * 
+ * Esta classe é responsável por criar e gerenciar os cards de tarefas.
+ */
 export default class Card {
   #taskList = [];
   #getTasks = [];
   #getConfigId;
 
+  /**
+   * Método createCard
+   * Cria um card com base nas configurações e tarefas fornecidas.
+   * @param {Object} configs - Configurações do card.
+   * @param {Array} tasks - Lista de tarefas associadas ao card.
+   * @returns {HTMLDivElement} - Elemento HTML do card criado.
+   */
   createCard(configs, tasks) {
     this.#getConfigId = configs.id;
     this.#getTasks = tasks;
@@ -38,7 +50,13 @@ export default class Card {
     cardDiv.appendChild(taskDiv);
     return cardDiv;
   }
-  
+
+  /**
+   * Método createTaskElement
+   * Cria um elemento de tarefa com base nos dados fornecidos.
+   * @param {Object} taskData - Dados da tarefa.
+   * @returns {HTMLDivElement} - Elemento HTML da tarefa criada.
+   */
   createTaskElement(taskData) {
     const taskElement = document.createElement('div');
     taskElement.setAttribute('draggable', 'true');
@@ -58,6 +76,12 @@ export default class Card {
     return taskElement;
   }
 
+  /**
+   * Método createElementInicialDateAndFinalDate
+   * Cria um elemento para exibição de datas iniciais e finais da tarefa.
+   * @param {Object} local - Dados da tarefa.
+   * @returns {HTMLDivElement} - Elemento HTML para datas.
+   */
   createElementInicialDateAndFinalDate(local) {
     const taskElementDate = document.createElement('div');
     taskElementDate.className = 'task-date';
@@ -65,6 +89,12 @@ export default class Card {
     return taskElementDate;
   }
 
+  /**
+   * Método createTaskElementPriority
+   * Cria um elemento para exibição da prioridade da tarefa.
+   * @param {Object} local - Dados da tarefa.
+   * @returns {HTMLDivElement} - Elemento HTML para prioridade.
+   */
   createTaskElementPriority(local) {
     const taskElementPriority = document.createElement('div');
     taskElementPriority.className = 'task-priority';
@@ -72,6 +102,12 @@ export default class Card {
     return taskElementPriority;
   }
 
+  /**
+   * Método createTaskElementDescription
+   * Cria um elemento para exibição da descrição da tarefa.
+   * @param {Object} local - Dados da tarefa.
+   * @returns {HTMLDivElement} - Elemento HTML para descrição.
+   */
   createTaskElementDescription(local) {
     const tasksElementDescription = document.createElement('div');
     tasksElementDescription.className = 'task-description';
@@ -84,6 +120,12 @@ export default class Card {
     return tasksElementDescription;
   }
 
+  /**
+   * Método createSubDivCard
+   * Cria um elemento de sub-divisão para o card.
+   * @param {string} label - Rótulo do card.
+   * @returns {HTMLDivElement} - Elemento HTML de sub-divisão.
+   */
   createSubDivCard(label) {
     const subCardDiv = document.createElement('div');
     subCardDiv.className = 'subdivcard';
@@ -93,6 +135,12 @@ export default class Card {
     return subCardDiv;
   }
 
+  /**
+   * Método createButtonHamburger
+   * Cria um botão de menu (hamburger) para o card.
+   * @param {string} id - Identificador do card.
+   * @returns {HTMLDivElement} - Elemento HTML do botão hamburger.
+   */
   createButtonHamburger(id) {
     const fatherNav = document.createElement('nav');
     fatherNav.className = 'fatherNav';
@@ -130,10 +178,21 @@ export default class Card {
     return fatherNav;
   }
 
+  /**
+   * Método openConfigCard
+   * Abre o menu de configuração do card.
+   * @param {HTMLDivElement} local - Elemento HTML do card.
+   * @param {string} id - Identificador do card.
+   */
   openConfigCard(local, id) {
     local.appendChild(this.createMenu(id));
   }
 
+  /**
+   * Método closeConfigCard
+   * Fecha o menu de configuração do card.
+   * @param {string} id - Identificador do card.
+   */
   closeConfigCard(id) {
     const menuReturn = document.querySelector(`#${id} .fatherMenu`);
     if (menuReturn) {
@@ -141,6 +200,12 @@ export default class Card {
     }
   }
 
+  /**
+   * Método createMenu
+   * Cria o menu de configuração do card.
+   * @param {string} id - Identificador do card.
+   * @returns {HTMLDivElement} - Elemento HTML do menu de configuração.
+   */
   createMenu(id) {
     const fatherMenu = document.createElement("div");
     fatherMenu.className = "fatherMenu";
@@ -151,21 +216,38 @@ export default class Card {
     return fatherMenu;
   }
 
+  /**
+   * Método onPDF
+   * Aciona a geração de um arquivo PDF com base nas tarefas do card.
+   */
   onPDF() {
     const pdfGenerator = new PDFGenerator(this.#getTasks, this.#getConfigId);
     pdfGenerator.generatePDF();
     pdfGenerator.closeWindow();
   }
 
+  /**
+   * Método onCSV
+   * Aciona a geração de um arquivo CSV com base nas tarefas do card.
+   */
   onCSV() {
     const csvGenerator = new CSVGenerator(this.#getTasks, this.#getConfigId);
     csvGenerator.generateCSV();
   }
 
+  /**
+   * Método moveToDoingTask
+   * Move uma tarefa para o estado "Em Andamento".
+   */
   moveToDoingTask() {
     console.log("passando para frente...");
   }
 
+  /**
+   * Método reloadTaskList
+   * Recarrega a lista de tarefas do card.
+   * @param {string} id - Identificador do card.
+   */
   reloadTaskList(id) {
     const isList = document.querySelector(`#${id} ul`);
     if (isList) {
@@ -176,6 +258,11 @@ export default class Card {
     this.addTask(local);
   }
 
+  /**
+   * Método addTask
+   * Adiciona uma nova tarefa à lista de tarefas do card.
+   * @param {HTMLDivElement} local - Elemento HTML do card.
+   */
   async addTask(local) {
     const simpleTask = new SimpleTask();
     const connection = new Connection();
@@ -188,6 +275,11 @@ export default class Card {
     local.appendChild(this.loadTaskList());
   }
 
+  /**
+   * Método loadTaskList
+   * Carrega a lista de tarefas no elemento HTML do card.
+   * @returns {HTMLDivElement} - Elemento HTML da lista de tarefas.
+   */
   loadTaskList() {
     const elementTask = document.getElementById('taskDiv');
 
@@ -198,6 +290,12 @@ export default class Card {
     return elementTask;
   }
 
+  /**
+   * Método configButton
+   * Configura os botões de ação do menu do card.
+   * @param {HTMLDivElement} local - Elemento HTML do menu.
+   * @param {string} id - Identificador do card.
+   */
   configButton(local, id) {
     const btnPDF = new Button();
     const btnCSV = new Button();
@@ -218,6 +316,12 @@ export default class Card {
     }
   }
 
+  /**
+   * Método getPriorityText
+   * Retorna a descrição da prioridade com base no valor numérico.
+   * @param {number} priority - Valor numérico representando a prioridade.
+   * @returns {string} - Descrição da prioridade.
+   */
   getPriorityText(priority) {
     return priority == 0 ? 'baixa' : priority == 1 ? 'média' : priority == 2 ? 'alta' : 'Não foi especificado o nível';
   }
--- Components/FileGenerator.js
+++ Components/FileGenerator.js
@@ -1,144 +1,219 @@
 import { styleTable } from "../Configuration/Configuration.js";
 import Util from "../Util.js";
 
+/**
+ * Classe CSVGenerator
+ * 
+ * Esta classe é responsável por gerar e baixar um arquivo CSV a partir de dados fornecidos.
+ */
 export class CSVGenerator {
-    constructor(getTasks, configId) {
-      this.getTasks = getTasks;
-      this.configId = configId;
-      this.util = new Util();
+  /**
+   * Construtor da classe CSVGenerator.
+   * @param {Function} getTasks - Função que retorna a lista de tarefas a serem incluídas no CSV.
+   * @param {string} configId - Identificador de configuração para filtrar as tarefas desejadas.
+   */
+  constructor(getTasks, configId) {
+    this.getTasks = getTasks;
+    this.configId = configId;
+    this.util = new Util();
+  }
+
+  /**
+   * Método generateCSV
+   * Gera um arquivo CSV a partir das tarefas obtidas pela função getTasks e realiza o download.
+   */
+  generateCSV() {
+    const filteredTasks = this.getTasks.filter(item => item.state_id == this.util.removeStringAndUnderline(this.configId));
+
+    if (filteredTasks.length > 0) {
+      const jsonData = filteredTasks.map(item => ({
+        "Tarefas": item.description,
+        "Estado das Tarefas": item.state_description,
+        "Prioridade das Tarefas": item.priority == 0 ? 'baixa' : item.priority == 1 ? 'media' : item.priority == 2 ? 'alta' : 0,
+        "Data de Inicio das Tarefas": item.initial_date,
+        "Data final das Tarefas": item.final_date,
+      }));
+
+      const csvData = this.convertToCSV(jsonData);
+      this.downloadCSV(csvData, 'documento.csv');
+    } else {
+      console.log("Nenhum dado encontrado para criar o CSV.");
     }
-  
-    generateCSV() {
-      const filteredTasks = this.getTasks.filter(item => item.state_id == this.util.removeStringAndUnderline(this.configId));
-      if (filteredTasks.length > 0) {
-        const jsonData = filteredTasks.map(item => ({
-          "Tarefas": item.description,
-          "Estado das Tarefas": item.state_description,
-          "Prioridade das Tarefas": item.priority == 0 ? 'baixa' : item.priority == 1 ? 'media' : item.priority == 2 ? 'alta' : 0,
-          "Data de Inicio das Tarefas": item.initial_date,
-          "Data final das Tarefas": item.final_date,
-        }));
-        const csvData = this.convertToCSV(jsonData);
-        this.downloadCSV(csvData, 'documento.csv');
-      } else {
-        console.log("Nenhum dado encontrado para criar o CSV.");
-      }
-    }
-  
-    convertToCSV(data) {
-      const header = Object.keys(data[0]).join('\t');
-      const rows = data.map(obj => Object.values(obj).join('\t'));
-      console.log(header, rows);
-  
-      return `${header}\n${rows.join('\n')}`;
-    }
-  
-    downloadCSV(csv, filename) {
-      const blob = new Blob([csv], { type: `text/csv;charset=utf-8, ${encodeURIComponent(csv)}` });
-      const url = URL.createObjectURL(blob);
-  
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = filename;
-  
-      document.body.appendChild(a);
-      a.click();
-  
-      document.body.removeChild(a);
-      URL.revokeObjectURL(url);
-    }
+  }
+
+  /**
+   * Método convertToCSV
+   * Converte os dados fornecidos em formato JSON para uma string CSV.
+   * @param {Array} data - Array de objetos contendo os dados a serem convertidos.
+   * @returns {string} - String CSV gerada a partir dos dados.
+   */
+  convertToCSV(data) {
+    const header = Object.keys(data[0]).join('\t');
+    const rows = data.map(obj => Object.values(obj).join('\t'));
+    console.log(header, rows);
+
+    return `${header}\n${rows.join('\n')}`;
+  }
+
+  /**
+   * Método downloadCSV
+   * Inicia o download do arquivo CSV gerado.
+   * @param {string} csv - String CSV a ser baixada.
+   * @param {string} filename - Nome do arquivo CSV a ser baixado.
+   */
+  downloadCSV(csv, filename) {
+    const blob = new Blob([csv], { type: `text/csv;charset=utf-8, ${encodeURIComponent(csv)}` });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  }
 }
 
+/**
+ * Classe PDFGenerator
+ * 
+ * Esta classe é responsável por gerar e imprimir um documento PDF a partir de dados fornecidos.
+ */
 export class PDFGenerator {
-    constructor(data, configId) {
-      this.content = document.createElement('div');
-      this.printWindow = window.open('', '_blank');
-      this.printDocument = this.printWindow.document;
-      this.head = document.createElement('head');
-      this.setupHead();
-      this.setupContent(data, configId);
-    }
-  
-    setupHead() {
-      const viewport = document.createElement('meta');
-      viewport.name = 'viewport';
-      viewport.content = 'width=device-width, initial-scale=1.0';
-      this.head.appendChild(viewport);
-      const charset = document.createElement('meta');
-      charset.charset = 'UTF-8';
-      this.head.appendChild(charset);
-      const style = document.createElement('style');
-      style.appendChild(document.createTextNode(styleTable));
-      this.head.appendChild(style);
-    }
-  
-    setupContent(data, configId) {
-      console.log(data);
-      const table = document.createElement('table');
-      const thead = document.createElement('thead');
-      const tbody = document.createElement('tbody');
-      thead.appendChild(this.createElementTr());
-      this.validate(tbody, configId, data);
-      table.appendChild(thead);
-      table.appendChild(tbody);
-      this.content.appendChild(table);
-    }
-
-    createElementTr() {
-      const headers = ['Tarefas', 'Estado das Tarefas', 'Prioridade', 'Data Inicial', 'Data Final', 'Percentual'];
-      const trHead = document.createElement('tr');
-      for (const headerText of headers) {
-          const th = document.createElement('th');
-          th.innerText = headerText;
-          trHead.appendChild(th);
-      }
-      return trHead;
+  /**
+   * Construtor da classe PDFGenerator.
+   * @param {Array} data - Dados a serem incluídos no documento PDF.
+   * @param {string} configId - Identificador de configuração para filtrar os dados desejados.
+   */
+  constructor(data, configId) {
+    this.content = document.createElement('div');
+    this.printWindow = window.open('', '_blank');
+    this.printDocument = this.printWindow.document;
+    this.head = document.createElement('head');
+    this.setupHead();
+    this.setupContent(data, configId);
   }
-  
+
+  /**
+   * Método setupHead
+   * Configura o cabeçalho do documento PDF.
+   */
+  setupHead() {
+    const viewport = document.createElement('meta');
+    viewport.name = 'viewport';
+    viewport.content = 'width=device-width, initial-scale=1.0';
+    this.head.appendChild(viewport);
+    const charset = document.createElement('meta');
+    charset.charset = 'UTF-8';
+    this.head.appendChild(charset);
+    const style = document.createElement('style');
+    style.appendChild(document.createTextNode(styleTable));
+    this.head.appendChild(style);
+  }
+
+  /**
+   * Método setupContent
+   * Configura o conteúdo do documento PDF com base nos dados fornecidos.
+   * @param {Array} data - Dados a serem incluídos no documento PDF.
+   * @param {string} configId - Identificador de configuração para filtrar os dados desejados.
+   */
+  setupContent(data, configId) {
+    console.log(data);
+    const table = document.createElement('table');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+    thead.appendChild(this.createElementTr());
+    this.validate(tbody, configId, data);
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    this.content.appendChild(table);
+  }
+
+  /**
+   * Método createElementTr
+   * Cria um elemento 'tr' para o cabeçalho da tabela no documento PDF.
+   * @returns {HTMLTableRowElement} - Elemento 'tr' do cabeçalho da tabela.
+   */
+  createElementTr() {
+    const headers = ['Tarefas', 'Estado das Tarefas', 'Prioridade', 'Data Inicial', 'Data Final', 'Percentual'];
+    const trHead = document.createElement('tr');
+    for (const headerText of headers) {
+      const th = document.createElement('th');
+      th.innerText = headerText;
+      trHead.appendChild(th);
+    }
+    return trHead;
+  }
+
+  /**
+   * Método validate
+   * Valida e preenche o corpo da tabela com os dados fornecidos.
+   * @param {HTMLTableSectionElement} tbody - Elemento 'tbody' da tabela.
+   * @param {string} configId - Identificador de configuração para filtrar os dados desejados.
+   * @param {Array} data - Dados a serem incluídos no documento PDF.
+   */
   validate(tbody, configId, data) {
     const util = new Util();
     const filteredTasks = data.filter(item => item.state_id == util.removeStringAndUnderline(configId));
     if (filteredTasks.length > 0) {
       filteredTasks.forEach(item => {
-          const trBody = document.createElement('tr');
-          const attributes = [
-              { key: 'description', label: 'tdDescription' },
-              { key: 'state_description', label: 'tdState' },
-              { key: 'priority', label: 'tdPriority', transform: value => this.getPriorityText(value)},
-              { key: 'initial_date', label: 'tdInitialDate', transform: value => util.formaDateUTF8(new Date(value)) }, 
-              { key: 'final_date', label: 'tdFinalDate', transform: value => util.formaDateUTF8(new Date(value)) },
-              { key: 'percent', label: 'tdPercent', transform: value => `${value}%` }
-          ];
-          attributes.forEach(attr => {
-              const td = document.createElement('td');
-              td.innerText = attr.transform ? attr.transform(item[attr.key]) : item[attr.key];
-              trBody.appendChild(td);
-          });
-          tbody.appendChild(trBody);
+        const trBody = document.createElement('tr');
+        const attributes = [
+          { key: 'description', label: 'tdDescription' },
+          { key: 'state_description', label: 'tdState' },
+          { key: 'priority', label: 'tdPriority', transform: value => this.getPriorityText(value)},
+          { key: 'initial_date', label: 'tdInitialDate', transform: value => util.formaDateUTF8(new Date(value)) }, 
+          { key: 'final_date', label: 'tdFinalDate', transform: value => util.formaDateUTF8(new Date(value)) },
+          { key: 'percent', label: 'tdPercent', transform: value => `${value}%` }
+        ];
+        attributes.forEach(attr => {
+          const td = document.createElement('td');
+          td.innerText = attr.transform ? attr.transform(item[attr.key]) : item[attr.key];
+          trBody.appendChild(td);
+        });
+        tbody.appendChild(trBody);
       });
     }
   }
 
+  /**
+   * Método getPriorityText
+   * Converte o valor numérico de prioridade em uma string descritiva.
+   * @param {number} priority - Valor numérico representando a prioridade.
+   * @returns {string} - String descritiva da prioridade.
+   */
   getPriorityText(priority) {
     return priority == 0 ? 'baixa' : priority == 1 ? 'média' : priority == 2 ? 'alta' : 'Não foi especificado o nível';
   }
 
+  /**
+   * Método generatePDF
+   * Gera o conteúdo HTML do documento PDF, imprime e fecha a janela de impressão.
+   */
   generatePDF() {
-      const htmlContent = `
-          <html>
-              ${this.head.outerHTML}
-              <body>
-                  ${this.content.outerHTML}
-              </body>
-          </html>
-      `;
-      this.printDocument.write(htmlContent);
-      this.printWindow.print();
-      this.printDocument.close();
+    const htmlContent = `
+      <html>
+        ${this.head.outerHTML}
+        <body>
+          ${this.content.outerHTML}
+        </body>
+      </html>
+    `;
+    this.printDocument.write(htmlContent);
+    this.printWindow.print();
+    this.printDocument.close();
   }
-    closeWindow() {
-        if (this.printWindow) {
-          this.printWindow.close();
-        }
+
+  /**
+   * Método closeWindow
+   * Fecha a janela de impressão.
+   */
+  closeWindow() {
+    if (this.printWindow) {
+      this.printWindow.close();
     }
   }
+}
