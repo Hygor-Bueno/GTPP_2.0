9bbda1b67869632533e960ed79614cfba03d0414
ProgramacaoPgPs <programacaopegpese@gmail.com>
2024-02-03 11:23:39 -0300

    send file

--- Class/SimpleTask.js
+++ Class/SimpleTask.js
@@ -62,17 +62,21 @@ export default class SimpleTask{
     }
 
     modalLauncher(result, taskList, local, funcAss) {
+        let inputRegisterTask = document.getElementById('registerInput');
+        let initialDate = document.getElementById('initialDate');
+        let finalDate = document.getElementById('finalDate');
+        let priority = document.getElementById('priority');
+        const modelos = [inputRegisterTask, initialDate, finalDate, priority];
         if(!result.error) {
             taskList.push({id: result.last_id, ...this});
             local.appendChild(funcAss());
             const modal = new Modal();
             modal.openModal('Tarefa registrada!', 'Parabéns uma nova tarefa já foi registrada com sucesso!', document.querySelector("#containerMain section"), 1);
         } else  {
-            let inputRegisterTask = document.getElementById('registerInput');
-            let initialDate = document.getElementById('initialDate');
-            let finalDate = document.getElementById('finalDate');
-            let priority = document.getElementById('priority');
-            [inputRegisterTask, initialDate, finalDate, priority].forEach((element) => {if(element.value.length <= 0) element.classList.add("borderRequired")})
+            modelos.forEach((element) => {
+                if(element.value.length <= 0) element.classList.add("borderRequired");
+                if(element.value.length > 0 || element.value !== '') element.classList.remove("borderRequired");
+            })
         }
     }
 
